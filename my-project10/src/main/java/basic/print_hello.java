4d4539a2c310f348c4b014ab734d71008b17a2d9
shubham <maneshubham599@gmail.com>
2024-05-16 18:05:11 +0530

    change full name

--- my-project10/src/main/java/basic/print_hello.java
+++ my-project10/src/main/java/basic/print_hello.java
@@ -4,7 +4,7 @@ public class print_hello {
 
 	public static void main(String [] args) {
 		
-		System.out.println("shubham mane");
+		System.out.println("shubham anup uma mane");
     
 		System.out.println("vaishali shubham mane");
 	}
